bd1cf719e8ddab5b60ea8c90d8544ff70f76f664
addshore <addshore@users.noreply.github.com>
2021-10-14 11:26:55 +0100

    Fix typos in ProductionCache.php

--- wbstack/src/Settings/ProductionCache.php
+++ wbstack/src/Settings/ProductionCache.php
@@ -10,7 +10,7 @@ $wgCdnServersNoPurge = [
 // TODO this can probably be removed now?
 $wgSquidServersNoPurge = $wgCdnServersNoPurge;
 
-$wgParserCacheType = 'db-replicated'; // 'db-replicated' is defined in LocalSetting.pjp currently
+$wgParserCacheType = 'db-replicated'; // 'db-replicated' is defined in LocalSettings.php currently
 
 // Don't specify a redis cache when running dbless maint script
 // TODO we probably do want a redis connection in some maint scripts...
