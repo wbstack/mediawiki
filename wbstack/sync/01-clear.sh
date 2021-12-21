28d5b6058aca422e9003fa17a862b23a90959b4c
addshore <addshorewiki@gmail.com>
2021-12-21 11:18:27 +0000

    Fix 01-clear, escaping |

--- wbstack/sync/01-clear.sh
+++ wbstack/sync/01-clear.sh
@@ -3,4 +3,4 @@
 
 echo "Clearing git repo (except the files that we maintain here)"
 
-find ./ -mindepth 1 ! -regex '^./\(\.dockerignore|\.git\|\.github\|\.hadolint\.yml\|wbstack\|docker\-compose\.yml\|Dockerfile\|robots\.txt\|health\.php\|composer\.\w+\.json\|LocalSettings\.php\)\(/.*\)?' -delete
+find ./ -mindepth 1 ! -regex '^./\(\.dockerignore\|\.git\|\.github\|\.hadolint\.yml\|wbstack\|docker\-compose\.yml\|Dockerfile\|robots\.txt\|health\.php\|composer\.\w+\.json\|LocalSettings\.php\)\(/.*\)?' -delete
