82411a36bf8d1c6c66d7d3f9b230f548d9dc62c2
addshore <addshorewiki@gmail.com>
2021-12-23 16:00:52 +0000

    Remove other not needed scripts

--- wbstack/sync/02-docker-fetch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-# Simply runs the fetch.sh script in a docker container
-docker run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v $PWD:/tmp --entrypoint sh ghcr.io/wbstack/docker-build-util:latest wbstack/sync/02-fetch.sh
--- wbstack/sync/updateFetchScript.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# This must be run in the sync directory!
-docker run --rm -v /$PWD/://tmp/src --entrypoint php php:7.4-cli //tmp/src/updateFetchScript.php
