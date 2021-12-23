9f5a60ba6ee0cb39e6846995b924c9bad7998f19
addshore <addshore@users.noreply.github.com>
2021-12-23 10:28:08 +0000

    Don't use a complex regex to clear files (#170)
    This has lead to errors in the past due to complex escaping.
    This new find format makes it much easier to see what is
    happened and far less error prone.

--- wbstack/sync/01-clear.sh
+++ wbstack/sync/01-clear.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env sh
-# Clears the git repo from fetched files and stuff..
 
-echo "Clearing git repo (except the files that we maintain here)"
+echo "Clearing most of the git repo (except the files we want to keep)"
 
-find ./ -mindepth 1 ! -regex '^./\(composer\.lock\|\.dockerignore\|\.git\|\.github\|\.hadolint\.yml\|wbstack\|docker\-compose\.yml\|Dockerfile\|robots\.txt\|health\.php\|composer\.\w+\.json\|LocalSettings\.php\)\(/.*\)?' -delete
+find ./ -mindepth 1 \
+    -not -path "./composer.lock" \
+    -not -path "./.dockerignore" \
+    -not -path "./.hadolint.yml" \
+    -not -path "./docker-compose.yml" \
+    -not -path "./Dockerfile" \
+    -not -path "./robots.txt" \
+    -not -path "./health.php" \
+    -not -path "./composer.*.json" \
+    -not -path "./LocalSettings.php" \
+    -not -path "./.git" -not -path "./.git/*" \
+    -not -path "./.github" -not -path "./.github/*" \
+    -not -path "./wbstack" -not -path "./wbstack/*" \
+    -delete
