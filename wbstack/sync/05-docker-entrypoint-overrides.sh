1dea6b11fc4c4a51a3013151356a2c1202c9aa79
addshore <addshorewiki@gmail.com>
2022-01-04 16:28:08 +0000

    Move files from wbstack dir into repo root

--- wbstack/sync/05-docker-entrypoint-overrides.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-# Simply runs the entrypoint-overrides.php script in a docker container
-docker run --rm -u $(id -u ${USER}):$(id -g ${USER}) -v $PWD:/app --entrypoint php php:7.4-cli /app/wbstack/sync/05-entrypoint-overrides.php
