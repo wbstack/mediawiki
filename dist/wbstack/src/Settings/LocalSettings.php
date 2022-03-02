6d3e7be738092edfa3ba279c0e88f5ad9dd5be64
Tobias <tobias.andersson@wikimedia.de>
2022-03-02 11:14:31 +0100

    Add wikibase scheme (#230)
    * Add MW_WIKIBASE_CONCEPTURI_SCHEME to Dockerfile
    
    Use this in localhost to set the concept uri scheme for rdf output.
    Hardcoding this was deemed better to reduce the complexity
    of injecting this config since we don't expect it to change

--- dist/wbstack/src/Settings/LocalSettings.php
+++ dist/wbstack/src/Settings/LocalSettings.php
@@ -513,7 +513,7 @@ $wgWBClientSettings['thisWikiIsTheRepo'] = true;
 $wgWBClientSettings['repoUrl'] = $GLOBALS['wgServer'];
 $wgWBClientSettings['repoSiteName'] = $GLOBALS['wgSitename'];
 
-$localConceptBaseUri = 'http://' . $wikiInfo->domain . '/entity/';
+$localConceptBaseUri = 'https://' . $wikiInfo->domain . '/entity/';
 
 $wgWBRepoSettings['entitySources'] = [
     'local' => 
