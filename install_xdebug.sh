ddab7fe1d7faaf106323f41a95c036fffefb8580
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2022-07-01 09:46:25 +0200

    fix line endings

--- install_xdebug.sh
+++ install_xdebug.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-pecl install xdebug \  
+pecl install xdebug \
     && docker-php-ext-enable xdebug  
   
-echo "zend_extension=xdebug\n\n\  
-[xdebug]\n\  
-xdebug.mode=develop,debug\n\  
-xdebug.client_host=host.minikube.internal\n\  
-xdebug.client_port=9003\n\  
+echo "zend_extension=xdebug
+[xdebug]
+xdebug.mode=develop,debug
+xdebug.client_host=host.minikube.internal
+xdebug.client_port=9003
 " > /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
