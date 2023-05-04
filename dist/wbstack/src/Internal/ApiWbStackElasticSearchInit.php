e5a5c88774743a87ed312451252fbce732dd2ff5
Andrew Kostka <andrew.kostka@wikimedia.de>
2023-05-04 15:14:27 +0000

    Run sync_dist-persist.sh

--- dist/wbstack/src/Internal/ApiWbStackElasticSearchInit.php
+++ dist/wbstack/src/Internal/ApiWbStackElasticSearchInit.php
@@ -13,28 +13,28 @@ class ApiWbStackElasticSearchInit extends \ApiBase {
         global $wgBaseDirectory;
 
         @set_time_limit( 60*5 ); // 5 mins maybe D:
-		@ini_set( 'memory_limit', '-1' ); // also try to disable the memory limit? Is this even a good idea?
+        @ini_set( 'memory_limit', '-1' ); // also try to disable the memory limit? Is this even a good idea?
 
-		$cluster = $this->getParameter('cluster');
+        $cluster = $this->getParameter('cluster');
 
-		$cmd = 'WBS_DOMAIN=' . $GLOBALS[WBSTACK_INFO_GLOBAL]->requestDomain . ' php ' . $wgBaseDirectory . '/extensions/CirrusSearch/maintenance/UpdateSearchIndexConfig.php --cluster ' . escapeshellarg( $cluster );
-		exec($cmd, $out, $return);
+        $cmd = 'WBS_DOMAIN=' . $GLOBALS[WBSTACK_INFO_GLOBAL]->requestDomain . ' php ' . $wgBaseDirectory . '/extensions/CirrusSearch/maintenance/UpdateSearchIndexConfig.php --cluster ' . escapeshellarg( $cluster );
+        exec($cmd, $out, $return);
 
-		// Return appropriate result
-		$res = [
-			'script' => 'extensions/CirrusSearch/maintenance/UpdateSearchIndexConfig.php',
-			'return' => $return,
-			'output' => $out,
-		];
-		$this->getResult()->addValue( null, $this->getModuleName(), $res );
+        // Return appropriate result
+        $res = [
+            'script' => 'extensions/CirrusSearch/maintenance/UpdateSearchIndexConfig.php',
+            'return' => $return,
+            'output' => $out,
+        ];
+        $this->getResult()->addValue( null, $this->getModuleName(), $res );
     }
 
-	public function getAllowedParams() {
-		return [
-			'cluster' => [
-				\ApiBase::PARAM_TYPE => 'string',
-				\ApiBase::PARAM_REQUIRED => true
-			]
-		];
-	}
+    public function getAllowedParams() {
+        return [
+            'cluster' => [
+                \ApiBase::PARAM_TYPE => 'string',
+                \ApiBase::PARAM_REQUIRED => true
+            ]
+        ];
+    }
 }
--- dist/wbstack/src/Internal/ApiWbStackForceSearchIndex.php
+++ dist/wbstack/src/Internal/ApiWbStackForceSearchIndex.php
@@ -13,23 +13,23 @@ class ApiWbStackForceSearchIndex extends \ApiBase {
         global $wgBaseDirectory;
 
         @set_time_limit( 60*5 ); // 5 mins maybe D:
-		@ini_set( 'memory_limit', '-1' ); // also try to disable the memory limit? Is this even a good idea?
+        @ini_set( 'memory_limit', '-1' ); // also try to disable the memory limit? Is this even a good idea?
 
         $fromId = $this->getParameter('fromId');
         $toId = $this->getParameter('toId');
         $cluster = $this->getParameter('cluster');
 
         $parameters = "--skipLinks 1 --indexOnSkip 1 --cluster " . escapeshellarg( $cluster ) . " --fromId " . escapeshellarg( $fromId ) . " --toId " . escapeshellarg( $toId );
-		$cmd = 'WBS_DOMAIN=' . $GLOBALS[WBSTACK_INFO_GLOBAL]->requestDomain . ' php ' . $wgBaseDirectory . '/extensions/CirrusSearch/maintenance/ForceSearchIndex.php ' . $parameters;
-		exec($cmd, $out, $return);
+        $cmd = 'WBS_DOMAIN=' . $GLOBALS[WBSTACK_INFO_GLOBAL]->requestDomain . ' php ' . $wgBaseDirectory . '/extensions/CirrusSearch/maintenance/ForceSearchIndex.php ' . $parameters;
+        exec($cmd, $out, $return);
 
-		// Return appropriate result
-		$res = [
-			'script' => 'extensions/CirrusSearch/maintenance/ForceSearchIndex.php',
-			'return' => $return,
-			'output' => $out,
-		];
-		$this->getResult()->addValue( null, $this->getModuleName(), $res );
+        // Return appropriate result
+        $res = [
+            'script' => 'extensions/CirrusSearch/maintenance/ForceSearchIndex.php',
+            'return' => $return,
+            'output' => $out,
+        ];
+        $this->getResult()->addValue( null, $this->getModuleName(), $res );
     }
 
     public function getAllowedParams() {
