f059f55d653421383d9851453d843fa7406cfae2
Andrew Kostka <andrew.kostka@wikimedia.de>
2023-05-08 13:33:36 +0000

    Update indentation in ApiWbStackQueueSearchIndexBatches.php

--- dist-persist/wbstack/src/Internal/ApiWbStackQueueSearchIndexBatches.php
+++ dist-persist/wbstack/src/Internal/ApiWbStackQueueSearchIndexBatches.php
@@ -13,21 +13,21 @@ class ApiWbStackQueueSearchIndexBatches extends \ApiBase {
         global $wgBaseDirectory;
 
         @set_time_limit( 60*5 ); // 5 mins maybe D:
-		@ini_set( 'memory_limit', '-1' ); // also try to disable the memory limit? Is this even a good idea?
+        @ini_set( 'memory_limit', '-1' ); // also try to disable the memory limit? Is this even a good idea?
 
         $cluster = $this->getParameter('cluster');
-		
-        $parameters = "--skipLinks --indexOnSkip --buildChunks 10000 --cluster " . escapeshellarg( $cluster );
-		$cmd = 'WBS_DOMAIN=' . $GLOBALS[WBSTACK_INFO_GLOBAL]->requestDomain . ' php ' . $wgBaseDirectory . '/extensions/CirrusSearch/maintenance/ForceSearchIndex.php ' . $parameters . ' 2>&1';
-		exec($cmd, $out, $return);
 
-		// Return appropriate result
-		$res = [
-			'script' => 'extensions/CirrusSearch/maintenance/ForceSearchIndex.php',
-			'return' => $return,
-			'output' => $out,
-		];
-		$this->getResult()->addValue( null, $this->getModuleName(), $res );
+        $parameters = "--skipLinks --indexOnSkip --buildChunks 10000 --cluster " . escapeshellarg( $cluster );
+        $cmd = 'WBS_DOMAIN=' . $GLOBALS[WBSTACK_INFO_GLOBAL]->requestDomain . ' php ' . $wgBaseDirectory . '/extensions/CirrusSearch/maintenance/ForceSearchIndex.php ' . $parameters . ' 2>&1';
+        exec($cmd, $out, $return);
+
+        // Return appropriate result
+        $res = [
+            'script' => 'extensions/CirrusSearch/maintenance/ForceSearchIndex.php',
+            'return' => $return,
+            'output' => $out,
+        ];
+        $this->getResult()->addValue( null, $this->getModuleName(), $res );
     }
 
     public function getAllowedParams() {
