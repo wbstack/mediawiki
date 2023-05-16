f7ae3292281427db7eb53c15940b68489d130800
Andrew Kostka <andrew.kostka@wikimedia.de>
2023-05-16 16:23:12 +0000

    Rename the primary Elasticsearch cluster to default
    CirrusSearch still attempts to connect to a default cluster even if one
    isn't specified in $wgCirrusSearchClusters. This results in uneeded jobs
    being created, which all inevitably fail due to the default cluster
    being nonexistent.

--- dist-persist/wbstack/src/Settings/LocalSettings.php
+++ dist-persist/wbstack/src/Settings/LocalSettings.php
@@ -637,7 +637,7 @@ if ( $wikiInfo->getSetting( 'wwExtEnableElasticSearch' ) ) {
     $wgCirrusSearchIndexBaseName = $wgDBname;
 
     $wgSearchType = 'CirrusSearch';
-    $wgCirrusSearchDefaultCluster = 'primary';
+    $wgCirrusSearchDefaultCluster = 'default';
     
     // T308115
     $wgCirrusSearchShardCount = [ 'content' => 1, 'general' => 1 ];
@@ -662,7 +662,7 @@ if ( $wikiInfo->getSetting( 'wwExtEnableElasticSearch' ) ) {
     }
 
     $wgCirrusSearchClusters = [
-        'primary' => getElasticClusterConfig( 'MW_PRIMARY_ELASTICSEARCH_' )
+        'default' => getElasticClusterConfig( 'MW_PRIMARY_ELASTICSEARCH_' )
     ];
 
     if ( getenv( 'MW_SECONDARY_ELASTICSEARCH_ENABLED' ) ) {
