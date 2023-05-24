7f10afddd6ea822bb01e627b371aa8a82d9dcb66
Deniz Erdogan <deniz.erdogan@wikimedia.de>
2023-05-24 10:33:07 +0200

    fix condition logic

--- dist-persist/wbstack/src/Settings/LocalSettings.php
+++ dist-persist/wbstack/src/Settings/LocalSettings.php
@@ -120,7 +120,7 @@ $wgDBservers = [
 // For example schema generation or localization cache reload
 // WBS_DOMAIN=maint php ./w/maintenance/update.php --schema sql.sql --quick
 // As in these contexts there is often no replica.... and thus one would fail...
-if( !$wwDomainIsMaintenance || empty(getenv('MW_DB_SERVER_REPLICA'))){
+if( !$wwDomainIsMaintenance && !empty(getenv('MW_DB_SERVER_REPLICA'))){
     $wgDBservers[] = [
         'host' => getenv('MW_DB_SERVER_REPLICA'),
         'dbname' => $wgDBname,
