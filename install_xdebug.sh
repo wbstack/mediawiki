461fb061fba902bd7ee60d7a100236a9696c4f8e
Andrew Kostka <andrew.kostka@wikimedia.de>
2023-08-25 12:09:03 +0200

    Configure Xdebug to be compatible with VSCode (#380)

--- install_xdebug.sh
+++ install_xdebug.sh
@@ -9,4 +9,5 @@ echo "zend_extension=xdebug
 xdebug.mode=develop,debug
 xdebug.client_host=host.minikube.internal
 xdebug.client_port=9003
+xdebug.start_with_request=yes
 " > /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
