e037a85b7778d430923a2018e29f48647fb10970
Andrew Kostka <andrew.kostka@wikimedia.de>
2024-12-10 11:43:57 +0000

    Revert vendor changes

--- dist/vendor/composer/autoload_static.php
+++ dist/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInite47817adb13a3b792b56ce584c8009e6
+class ComposerStaticInit363f9183335017aee6b02c5b8326cd74
 {
     public static $files = array (
         '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
@@ -4249,11 +4249,11 @@ class ComposerStaticInite47817adb13a3b792b56ce584c8009e6
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInite47817adb13a3b792b56ce584c8009e6::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInite47817adb13a3b792b56ce584c8009e6::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInite47817adb13a3b792b56ce584c8009e6::$prefixesPsr0;
-            $loader->fallbackDirsPsr0 = ComposerStaticInite47817adb13a3b792b56ce584c8009e6::$fallbackDirsPsr0;
-            $loader->classMap = ComposerStaticInite47817adb13a3b792b56ce584c8009e6::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit363f9183335017aee6b02c5b8326cd74::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit363f9183335017aee6b02c5b8326cd74::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit363f9183335017aee6b02c5b8326cd74::$prefixesPsr0;
+            $loader->fallbackDirsPsr0 = ComposerStaticInit363f9183335017aee6b02c5b8326cd74::$fallbackDirsPsr0;
+            $loader->classMap = ComposerStaticInit363f9183335017aee6b02c5b8326cd74::$classMap;
 
         }, null, ClassLoader::class);
     }
