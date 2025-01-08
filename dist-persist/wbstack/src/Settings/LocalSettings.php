a390cff3edc08feab0660776888856a0461a95d6
dena <dena@wikimedia.de>
2025-01-08 13:17:16 +0100

    adjust local http switch only for docker-compose

--- dist-persist/wbstack/src/Settings/LocalSettings.php
+++ dist-persist/wbstack/src/Settings/LocalSettings.php
@@ -29,6 +29,7 @@ $wwDomainIsMaintenance = $wikiInfo->requestDomain === 'maintenance';
 $wwIsPhpUnit = isset( $maintClass ) && $maintClass === 'PHPUnitMaintClass';
 $wwIsLocalisationRebuild = basename( $_SERVER['SCRIPT_NAME'] ) === 'rebuildLocalisationCache.php';
 $wwLocalization = new Localization( $wgExtensionMessagesFiles, $wgMessagesDirs, $wgBaseDirectory, $wwIsLocalisationRebuild );
+$wwDockerCompose = getenv('WBSTACK_DOCKER_COMPOSE') === 'yes';
 
 $wwUseMailgunExtension = true; // default for wbstack
 if (getenv('MW_MAILGUN_DISABLED') === 'yes') {
@@ -93,7 +94,7 @@ if ( getenv('MW_LOG_TO_STDERR') === 'yes' ) {
     ];
 }
 
-if ( $wwDomainSaysLocal ) {
+if ( $wwDockerCompose ) {
     $wgServer = "http://" . $wikiInfo->domain;
 } else {
     $wgServer = "https://" . $wikiInfo->domain;
